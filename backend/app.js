5542a3491180718e779cae7d5a1b18c078702ed5
fernand0dejesus <20200335@ricaldone.edu.sv>
2025-05-29 13:53:21 -0600

    efer

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,18 @@ import logoutRoutes from "./src/routes/logout.js";
 import registerClientRoutes from "./src/routes/registerClients.js";
 import recoveryPasswordRoutes from "./src/routes/recoveryPassword.js";
 import providersRoutes from "./src/routes/providers.js";
-
+import cors from "cors"
 // Creo una constante que es igual a la libreria que importé
 const app = express();
 
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      // Permitir envío de cookies y credenciales
+      credentials: true
+    })
+  );
+  
 //Que acepte datos en json
 app.use(express.json());
 //Que postman acepte guardar cookies
